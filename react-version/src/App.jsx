b7d36e39b604b6c4cc08eb3a209d7805beac6723
Sm Kou <66178856+SmKou@users.noreply.github.com>
2023-10-08 23:27:58 -0700

    Add input fields and conversion functions

--- react-version/src/App.jsx
+++ react-version/src/App.jsx
@@ -1,35 +1,66 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { convert } from './convert'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [tempInFahrenheit, setTempInFahrenheit] = useState(32.0)
+    const [tempInCelsius, setTempInCelsius] = useState(0.0)
+    const [tempInKelvin, setTempInKelvin] = useState(273.15)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const getInput = (val, delta) => {
+        if (!val)
+            return;
+
+        let n = 0
+        try { n = parseFloat(val) } 
+        catch {
+            if (delta === 'fahrenheit')
+                n = 32.0
+            else if (delta === 'kelvin')
+                n = 273.15
+        }
+
+        const temps = convert(n, delta)
+        setTempInFahrenheit(temps.fahrenheit)
+        setTempInCelsius(temps.celsius)
+        setTempInKelvin(temps.kelvin)
+    }
+
+    return (
+        <>
+            <h1>Temperature Converter</h1>
+            <hr />
+            <div className="ipt-set">
+                <label>
+                    Fahrenheit:
+                    <input 
+                        type="text" 
+                        value={tempInFahrenheit} 
+                        onFocus={() => setTempInFahrenheit('')}
+                        onChange={e => getInput(e.target.value, 'fahrenheit')}
+                    />
+                 </label>
+                 <label>
+                    Celsius:
+                    <input 
+                        type="text" 
+                        value={tempInCelsius}
+                        onFocus={() => setTempInCelsius('')}
+                        onChange={e => getInput(e.target.value, 'celsius')}
+                    />
+                 </label>
+                 <label>
+                    Kelvin:
+                    <input 
+                        type="text"
+                        value={tempInKelvin}
+                        onFocus={() => setTempInKelvin('')}
+                        onChange={e => getInput(e.target.value, 'kelvin')}
+                    />
+                 </label>
+            </div>
+        </>
+    )
 }
 
 export default App
